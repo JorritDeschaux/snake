6dfad1f9e8e87fe91063cfee6a4c0ecece30c501
Jorrit Deschaux <jorritdesch@gmail.com>
2024-03-11 20:01:28 +0100

    main and snake component

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// extern crate rand;
+// extern crate piston_window;
+
+mod snake;
+
+use opengl_graphics::{GlGraphics, OpenGL};
+
+struct Window {
+    gl: GlGraphics, 
+}
+
+impl Window {
+    // fn render()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (width, height) = (40, 4);
+
 }
---
+++ src/snake.rs
@@ -0,0 +1,6 @@
+struct Snake {
+    length: u16,
+    speed: u16,
+    alive: bool
+}
+
